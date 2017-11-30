6a33943fd52bae98fc82ad59b9d9632ae671f0ee
AnastasiiaSoloveva <floflo134@gmail.com>
2017-12-01 02:23:08 +0300

    beanFactoryPostProcessor added

--- src/main/java/quoters/Main.java
+++ src/main/java/quoters/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args)  {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-
+        context.getBean(Quoter.class).sayQuote();
 
     }
 }
