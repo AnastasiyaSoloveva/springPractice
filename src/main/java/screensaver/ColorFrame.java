d90805f12c0c476be66631075329c845b5191f9b
AnastasiiaSoloveva <floflo134@gmail.com>
2017-12-01 10:19:03 +0300

    colorFrame bean added

--- src/main/java/screensaver/ColorFrame.java
+++ src/main/java/screensaver/ColorFrame.java
@@ -8,10 +8,8 @@ import java.awt.*;
 import java.util.Random;
 
 @Component
-public class ColorFrame extends JFrame {
 
-    @Autowired
-    private Color color;
+public abstract class ColorFrame extends JFrame {
 
     public ColorFrame() throws HeadlessException {
         setSize(200,200);
@@ -22,8 +20,10 @@ public class ColorFrame extends JFrame {
     public void showOnRandomPlace(){
         Random random = new Random();
         setLocation(random.nextInt(1200),random.nextInt(700));
-        getContentPane().setBackground(color);
+        getContentPane().setBackground(getColor());
         repaint();
 
     }
+
+    protected abstract Color getColor();
 }
